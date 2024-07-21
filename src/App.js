8b0f151bc7514caaacf2a608f6142ecd553ba377
jhuido <josefinahuidobro@uca.edu.ar>
2024-07-21 17:31:16 -0300

    adding tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Home from "./modules/Home";
+
+function App() {
+  return (
+    <div className="bg-slate-900">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/Home.js
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div className="bg-slate-900">
+        <h1>Hola como estas</h1>
+        <p>todo bien y vos</p>
+    </div>
+  );
+}
+
+export default Home;
